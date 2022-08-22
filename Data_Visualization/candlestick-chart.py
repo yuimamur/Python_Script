96eb8a38dcc88f76c6143854b77dffe5b4d4f44f
yuimamur <59761194+yuimamur@users.noreply.github.com>
2022-08-22 13:57:20 +0900

    Create candlestick-chart.py

---
+++ Data_Visualization/candlestick-chart.py
@@ -0,0 +1,14 @@
+# pip3 install pandas,plotly
+
+import pandas as pd
+data = pd.read_csv("AAPL.csv")
+import plotly.graph_objects as go
+figure = go.Figure(data=[go.Candlestick(x=data["Date"],
+                                        open=data["Open"], 
+                                        high=data["High"],
+                                        low=data["Low"], 
+                                        close=data["Close"])])
+figure.update_layout(title = "Apple Stock Price Analysis", 
+                     xaxis_rangeslider_visible=False)
+figure.show()
+
