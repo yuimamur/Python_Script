73c51f8ea894e626ed7527c4eb48de2cc66e17bc
yuimamur <59761194+yuimamur@users.noreply.github.com>
2022-08-22 13:58:41 +0900

    Update candlestick-chart.py

--- Data_Visualization/candlestick-chart.py
+++ Data_Visualization/candlestick-chart.py
@@ -1,7 +1,7 @@
 # pip3 install pandas,plotly
 
 import pandas as pd
-data = pd.read_csv("AAPL.csv")
+data = pd.read_csv("test.csv")
 import plotly.graph_objects as go
 figure = go.Figure(data=[go.Candlestick(x=data["Date"],
                                         open=data["Open"], 
